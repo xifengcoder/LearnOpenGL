bd7aee0df001132eab65af42c53afa90e7d6874e
xifeng.yang <yxf05427@163.com>
2023-09-10 08:17:06 +0800

    Initial Commit

---
+++ LearnOpenGL/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LearnOpenGL
+//
+//  Created by ChrisYang on 2023/9/10.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
